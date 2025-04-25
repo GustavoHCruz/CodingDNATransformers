289d44b04d966ab65a8516d266214fc81b6efed5
Gustavo Henrique <ghfc2000@hotmail.com>
2025-04-25 16:52:55 -0300

    fixes e gencode exin

--- src/backend/services/dataset_service.py
+++ src/backend/services/dataset_service.py
@@ -1,8 +1,10 @@
 import threading
 from datetime import datetime
+from typing import Literal
 
 from etl.genbank import (exin_classifier_gb, exin_translator_gb,
                          protein_translator_gb, sliding_window_tagger_gb)
+from etl.gencode import exin_classifier_gc
 from models.parent_dataset_model import ApproachEnum, OriginEnum, ParentDataset
 from models.progress_tracker_model import ProgressTypeEnum
 from schemas.datasets_schema import CreationSettings, CreationSettingsResponse
@@ -12,125 +14,155 @@ from services.parent_dataset_service import (
 from services.parent_record_service import \
     bulk_create_parent_record_from_generator
 from services.progress_tracker_service import create_progress
-from services.raw_file_info_service import get_by_file_name
+from services.raw_file_info_service import (get_by_file_name_and_approach,
+                                            save_file)
 
+
+def initial_configs(path: str, approach: ApproachEnum) -> tuple[int, Literal[ProgressTypeEnum.percentage, ProgressTypeEnum.counter]]:
+	raw_file_info = get_by_file_name_and_approach(path, approach)
+
+	total_records = None
+	progress_type = ProgressTypeEnum.counter
+	if raw_file_info:
+		total_records = raw_file_info.total_records
+		progress_type = ProgressTypeEnum.percentage
+
+	return total_records, progress_type
 
 @handle_exceptions
 def process_raw(settings: CreationSettings) -> CreationSettingsResponse:
 	response = CreationSettingsResponse()
+	batch_size = 500
 		
 	if settings.genbank:
-		raw_file_info = get_by_file_name("data/raw/genbank/file1.gb")
-
-		new_reading = True
-		total_records = 0
-		progress_type = ProgressTypeEnum.counter
-		if raw_file_info:
-			total_records = raw_file_info.total_records
-			new_reading = False
-			progress_type = ProgressTypeEnum.percentage
+		genbank_file_path = "data/raw/genbank/file1.gb"
 
 		if settings.genbank.ExInClassifier:
-			progress_tracker = create_progress(progress_type)
-			response.genbank.ExInClassifier = progress_tracker.id
+			approach = ApproachEnum.exin_classifier
+			total_records, progress_type = initial_configs(genbank_file_path, approach)
 
-			parent_dataset = ParentDataset(
-				name=f"ExInClassifier-{datetime.now()}",
-				approach=ApproachEnum.exin_classifier,
+			parent_id = create_parent_dataset(ParentDataset(
+				name=f"{approach}-{datetime.now()}",
+				approach=approach,
 				origin=OriginEnum.genbank
-			)
+			)).id
 
-			parent_dataset = create_parent_dataset(parent_dataset)
-
+			task_id = create_progress(progress_type, f"{approach}-{parent_id}").id
+			response.genbank.ExInClassifier = task_id
 			def background_exin_classifier_gb() -> None:
-				records_generator = exin_classifier_gb("data/raw/genbank/file1.gb", total_records, new_reading, progress_tracker.id, parent_dataset.id)
+				records_generator = exin_classifier_gb(genbank_file_path, parent_id)
 
-				total = bulk_create_parent_record_from_generator(records_generator)
+				accepted_total, raw_total = bulk_create_parent_record_from_generator(records_generator, batch_size, task_id, total_records)
 
-				update_parent_dataset_record_counter(parent_dataset.id, total)
+				update_parent_dataset_record_counter(parent_id, accepted_total)
+
+				if not total_records:
+					save_file(genbank_file_path, approach, raw_total)
 
 			threading.Thread(target=background_exin_classifier_gb).start()
 		
 		if settings.genbank.ExInTranslator:
-			progress_tracker = create_progress(progress_type)
-			response.genbank.ExInTranslator = progress_tracker.id
+			approach = ApproachEnum.exin_translator
+			total_records, progress_type = initial_configs(genbank_file_path, approach)
 
-			parent_dataset = ParentDataset(
-				name=f"ExInTranslator-{datetime.now()}",
-				approach=ApproachEnum.exin_translator,
+			parent_id = create_parent_dataset(ParentDataset(
+				name=f"{approach}-{datetime.now()}",
+				approach=approach,
 				origin=OriginEnum.genbank
-			)
+			)).id
 
-			parent_dataset = create_parent_dataset(parent_dataset)
+			task_id = create_progress(progress_type, f"{approach}-{parent_id}").id
+			response.genbank.ExInTranslator = task_id
 
 			def background_exin_translator_gb() -> None:
-				records_generator = exin_translator_gb("data/raw/genbank/file1.gb", total_records, new_reading, progress_tracker.id, parent_dataset.id)
+				records_generator = exin_translator_gb(genbank_file_path, parent_id)
 
-				total = bulk_create_parent_record_from_generator(records_generator)
+				accepted_total, raw_total = bulk_create_parent_record_from_generator(records_generator, batch_size, task_id, total_records)
 
-				update_parent_dataset_record_counter(parent_dataset.id, total)
+				update_parent_dataset_record_counter(parent_id, accepted_total)
+
+				if not total_records:
+					save_file(genbank_file_path, approach, raw_total)
 			
 			threading.Thread(target=background_exin_translator_gb).start()
 		
 		if settings.genbank.SlidingWindowTagger:
-			progress_tracker = create_progress(progress_type)
-			response.genbank.SlidingWindowTagger = progress_tracker.id
+			approach = ApproachEnum.sliding_window_extraction
+			total_records, progress_type = initial_configs(genbank_file_path, approach)
 
-			parent_dataset = ParentDataset(
-				name=f"SlidingWindowTagger-{datetime.now()}",
-				approach=ApproachEnum.sliding_window_extraction,
+			parent_id = create_parent_dataset(ParentDataset(
+				name=f"{approach}-{datetime.now()}",
+				approach=approach,
 				origin=OriginEnum.genbank
-			)
+			)).id
 
-			parent_dataset = create_parent_dataset(parent_dataset)
+			task_id = create_progress(progress_type, f"{approach}-{parent_id}").id
+			response.genbank.SlidingWindowTagger = task_id
 
 			def background_sliding_window_tagger_gb() -> None:
-				records_generator = sliding_window_tagger_gb("data/raw/genbank/file1.gb", total_records, new_reading, progress_tracker.id, parent_dataset.id)
+				records_generator = sliding_window_tagger_gb(genbank_file_path, parent_id)
 
-				total = bulk_create_parent_record_from_generator(records_generator)
+				accepted_total, raw_total = bulk_create_parent_record_from_generator(records_generator, batch_size, task_id, total_records)
 
-				update_parent_dataset_record_counter(parent_dataset.id, total)
+				update_parent_dataset_record_counter(parent_id, accepted_total)
+
+				if not total_records:
+					save_file(genbank_file_path, approach, raw_total)
 			
 			threading.Thread(target=background_sliding_window_tagger_gb).start()
 		
 		if settings.genbank.ProteinTranslator:
-			progress_tracker = create_progress(progress_type)
-			response.genbank.ProteinTranslator = progress_tracker.id
+			approach = ApproachEnum.protein_translator
+			total_records, progress_type = initial_configs(genbank_file_path, approach)
 
-			parent_dataset = ParentDataset(
-				name=f"ProteinTranslator-{datetime.now()}",
-				approach=ApproachEnum.protein_translator,
+			parent_id = create_parent_dataset(ParentDataset(
+				name=f"{approach}-{datetime.now()}",
+				approach=approach,
 				origin=OriginEnum.genbank
-			)
+			)).id
 
-			parent_dataset = create_parent_dataset(parent_dataset)
+			task_id = create_progress(progress_type, f"{approach}-{parent_id}").id
+			response.genbank.ProteinTranslator = task_id
 
 			def background_protein_translator_gb() -> None:
-				records_generator = protein_translator_gb("data/raw/genbank/file1.gb", total_records, new_reading, progress_tracker.id, parent_dataset.id)
+				records_generator = protein_translator_gb(genbank_file_path, parent_id)
 
-				total = bulk_create_parent_record_from_generator(records_generator)
+				accepted_total, raw_total = bulk_create_parent_record_from_generator(records_generator, batch_size, task_id, total_records)
 
-				update_parent_dataset_record_counter(parent_dataset.id, total)
-			
+				update_parent_dataset_record_counter(parent_id, accepted_total)
+
+				if not total_records:
+					save_file(genbank_file_path, approach, raw_total)
+
 			threading.Thread(target=background_protein_translator_gb).start()
 
 	if settings.gencode:
-		raw_file_info = get_by_file_name("data/raw/gencode/file1.fa")
-
-		new_reading = True
-		total_records = 0
-		progress_type = ProgressTypeEnum.counter
-		if raw_file_info:
-			total_records = raw_file_info.total_records
-			new_reading = False
-			progress_type = ProgressTypeEnum.percentage
+		gencode_fasta_path = "data/raw/gencode/file1.fa"
+		gencode_annotations_path = "data/raw/gencode/file1.gtf"
 		
 		if settings.gencode.ExInClassifier:
-			progress_tracker = create_progress(type=progress_type)		
-			response.gencode.ExInClassifier = progress_tracker.id
+			approach = ApproachEnum.exin_classifier
+			total_records, progress_type = initial_configs(gencode_annotations_path, approach)
 
-			# call ExInClassifier(progress_tracker.id) - gencode
+			parent_id = create_parent_dataset(ParentDataset(
+				name=f"{approach}-{datetime.now()}",
+				approach=approach,
+				origin=OriginEnum.gencode
+			)).id
 
-			response.gencode.ExInClassifier = progress_tracker.id
+			task_id = create_progress(progress_type, f"ExInClassifier-{parent_id}").id		
+			response.gencode.ExInClassifier = task_id
+
+			def background_exin_classifier_gc() -> None:
+				records_renerator = exin_classifier_gc(gencode_fasta_path, gencode_annotations_path, parent_id)
+
+				accepted_total, raw_total = bulk_create_parent_record_from_generator(records_renerator, batch_size, task_id, total_records)
+
+				update_parent_dataset_record_counter(parent_id, accepted_total)
+
+				if not total_records:
+					save_file(gencode_annotations_path, approach, raw_total)
+
+			threading.Thread(target=background_exin_classifier_gc).start()
 
 	return response
